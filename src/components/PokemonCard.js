ef8e37029328c4fa01367fa37c7eca4c493677d7
MichalPlatko <miso.platko@gmail.com>
2021-03-21 03:39:42 +0100

    custom pokemon card component which is able to display photo,name,id, and types of specific pokemon(wip)

---
+++ src/components/PokemonCard.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+import "../css/PokemonCard.scss";
+import {TypeBadge as Badge,BadgeTheme} from "./TypeBadge";
+
+function PokemonCard({id,photo,name,badges}) {
+    return (
+      
+        <div className="pokemon-card">
+        <div className="pokemon-photo"><img src={photo} width={120} height={120}></img></div>
+        <div className="pokemon-id">#{id}</div>
+        <div className="pokemon-name">
+        <Link to={`/pokemon/${name}`} style={{textDecoration:"none"}}>
+        {name}
+        </Link>
+        </div>
+        <div className="pokemon-types">
+        <Link to={`/types/grass`} style={{textDecoration:"none"}}>
+        <Badge typeName="grass" color={BadgeTheme["grass"]}/>
+        </Link>
+        <div className="second-type">
+        <Link to={`/types/${badges.type}`} style={{textDecoration:"none"}}>
+        <Badge typeName={badges.type} color={BadgeTheme[badges.type]}/>
+        </Link>        
+        </div>
+
+        
+        </div>
+        </div>
+    )
+}
+
+export default PokemonCard;
