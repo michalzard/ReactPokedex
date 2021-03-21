678bee875b8a9e755a7dc9c1c502d531b1434aea
MichalPlatko <miso.platko@gmail.com>
2021-03-21 03:37:44 +0100

    simple header with logo & search component

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import "../css/Header.scss"
+import Pokeball from "../assets/pokeball.png";
+import {TextField,Button} from "@material-ui/core";
+import SearchIcon from '@material-ui/icons/SearchSharp';
+function Header() {
+    return (
+        <div className="header">
+        <div className="header-logo"></div>
+        <div className="header-title">
+        <img src={Pokeball} alt="logo" width={40}></img>
+        <span>Poké Catalog</span>
+        </div>
+        <div className="header-search"><TextField color="secondary" placeholder="Pokémon name,type,ability"
+        variant="outlined" size="small"  label="Search" inputProps={{
+        style:{color:"white"}}} InputLabelProps={{style:{color:"white"}}}/>
+        <Button  className="header-search-button" variant="contained" color="secondary"><SearchIcon/></Button>
+        </div>
+        </div>
+    )
+}
+
+export default Header
