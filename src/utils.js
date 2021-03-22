9ebf96c22edf3911d2443f5f4866e415353e40f3
MichalPlatko <miso.platko@gmail.com>
2021-03-22 15:10:19 +0100

    simple way to load picutres from folder inside of react

---
+++ src/utils.js
@@ -0,0 +1,15 @@
+function importAll(r) {
+    return r.keys().map(r);
+  }
+  
+function getImagesByName(name){
+const images = importAll(require.context('./assets/pokemon', false, /\.(ico)$/));
+for(let i=0;i<images.length;i++){
+    if(images[i].default.toLowerCase().includes(name)){
+        return images[i].default;
+    }else continue;
+}
+}
+
+
+export {getImagesByName};
