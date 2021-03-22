da01aa9faed384558f0f5c52390b837ecd38c3b8
MichalPlatko <miso.platko@gmail.com>
2021-03-22 15:11:19 +0100

    fetching pokeapi to get data about all pokemons in json

--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -1,17 +1,31 @@
-import React from 'react'
+import React,{useEffect, useState,useRef} from 'react'
 import "../css/CardContainer.scss";
 import PokemonCard from './PokemonCard';
-import Logo from "../assets/p.png";
+
+import {getImagesByName} from "../utils";
+
+
 //import {Button} from "@material-ui/core";
 
 function CardContainer() {
-    const arr=[];
-    for(let i=0;i<50;i++){arr.push("p"+Math.random())}
+    const [results,setResults] = useState([]);
+    let isMounted=useRef(true);
+    useEffect(()=>{
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=492")
+    .then(res=>res.json()).then(res=>{
+            if(isMounted.current) setResults(res.results);
+            //Save to local sotrage cuz shit expensive to fetch all the time
+    })
+    return ()=>{isMounted.current=false;}
+    },[])
+
     return (
         <div className="cards-container">
         <div className="cards-catalog-left"></div>
         <div className="cards-catalog">
-        {arr.map((index)=>{ return <PokemonCard key={index} id="001" photo={Logo} name="Firesaur" badges={{type:"flying"}}/>})}  
+        {results.map((result,i)=>{
+        return <PokemonCard key={i} id={i}
+        photo={getImagesByName(result.name)} name={result.name}/>})}  
         </div>
 
         <div className="cards-catalog-right"></div>
