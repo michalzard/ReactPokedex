033442208923ba57c7acf45e7d5896238db8a87f
MichalPlatko <miso.platko@gmail.com>
2021-03-24 12:47:54 +0100

    changed flex-direction,put in overflow for whole content

--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -1,6 +1,7 @@
 import React,{useEffect, useState,useRef} from 'react'
 import "../css/CardContainer.scss";
 import PokemonCard from './PokemonCard';
+import Footer from "./Footer";
 
 import {getImagesByName} from "../utils";
 
@@ -8,12 +9,13 @@ import {getImagesByName} from "../utils";
 //import {Button} from "@material-ui/core";
 
 function CardContainer() {
+    
     const [results,setResults] = useState([]);
     let isMounted=useRef(true);
     useEffect(()=>{
     fetch("https://pokeapi.co/api/v2/pokemon?limit=492")
     .then(res=>res.json()).then(res=>{
-            if(isMounted.current) setResults(res.results);
+            if(isMounted.current)setResults(res.results);
             //Save to local sotrage cuz shit expensive to fetch all the time
     })
     return ()=>{isMounted.current=false;}
@@ -27,8 +29,8 @@ function CardContainer() {
         return <PokemonCard key={i} id={i}
         photo={getImagesByName(result.name)} name={result.name}/>})}  
         </div>
-
         <div className="cards-catalog-right"></div>
+        <div className="footer"><Footer/></div>
         </div>
     )
 }
