11995a73831eec5178d7743545b3e130273ff053
MichalPlatko <miso.platko@gmail.com>
2021-03-25 02:13:00 +0100

    localstorage "caching"

--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -5,19 +5,30 @@ import Footer from "./Footer";
 
 import {getImagesByName} from "../utils";
 
+import { useSelector } from 'react-redux';
+import appSlice from "./store/appSlice";
 
 //import {Button} from "@material-ui/core";
 
+
 function CardContainer() {
-    
+    const inputFromRedux=useSelector(appSlice.actions.setInputValue);
     const [results,setResults] = useState([]);
+    //const [input,setInput] = useState("");
     let isMounted=useRef(true);
     useEffect(()=>{
+    if(!localStorage.getItem("pokemon")){
     fetch("https://pokeapi.co/api/v2/pokemon?limit=492")
     .then(res=>res.json()).then(res=>{
-            if(isMounted.current)setResults(res.results);
-            //Save to local sotrage cuz shit expensive to fetch all the time
-    })
+            if(isMounted.current){
+                setResults(res.results);
+                localStorage.setItem("pokemon",JSON.stringify(res.results))
+                console.log("results fetched & saved")
+            }
+    })}else {
+        setResults(JSON.parse(localStorage.getItem("pokemon")))
+        console.log("results loaded")
+    }
     return ()=>{isMounted.current=false;}
     },[])
 
@@ -26,8 +37,9 @@ function CardContainer() {
         <div className="cards-catalog-left"></div>
         <div className="cards-catalog">
         {results.map((result,i)=>{
-        return <PokemonCard key={i} id={i}
-        photo={getImagesByName(result.name)} name={result.name}/>})}  
+        return  result.name.includes(inputFromRedux.payload.searchInput) ?
+        <PokemonCard key={i} id={i}
+        photo={getImagesByName(result.name)} name={result.name}/> : null})}  
         </div>
         <div className="cards-catalog-right"></div>
         <div className="footer"><Footer/></div>
