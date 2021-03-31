092f01482916b75455e1578c7f36c05995b82f43
MichalPlatko <miso.platko@gmail.com>
2021-03-31 17:53:07 +0200

    load more button added,removed local storage saving

--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -4,7 +4,7 @@ import PokemonCard from './PokemonCard';
 import Footer from "./Footer";
 
 import {getImagesByName} from "../utils";
-
+import {Button} from "@material-ui/core";
 import { useSelector } from 'react-redux';
 import appSlice from "./store/appSlice";
 
@@ -14,35 +14,35 @@ import appSlice from "./store/appSlice";
 function CardContainer() {
     const inputFromRedux=useSelector(appSlice.actions.setInputValue);
     const [results,setResults] = useState([]);
-    //const [input,setInput] = useState("");
+    const maxVisibleResults=897; //after 897 there are weird affixes on names
+    const [visibleResults,setVisibleResults]= useState(32);
     let isMounted=useRef(true);
     useEffect(()=>{
     async function fetchPokeData(){
-        if(!localStorage.getItem("pokemon-v2")){
-            await fetch("https://pokeapi.co/api/v2/pokemon?limit=898")
-            .then(res=>res.json()).then(res=>{
-                    if(isMounted.current){
-                        setResults(res.results);
-                        localStorage.setItem("pokemon-v2",JSON.stringify(res.results))
-                    }
+    await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${maxVisibleResults}`)
+    .then(res=>res.json()).then(res=>{
+    if(isMounted.current){
+    setResults(res.results);
     }
-    
-    )}else {
-        setResults(JSON.parse(localStorage.getItem("pokemon-v2")))
     }
-    return ()=>{isMounted.current=false;}
-    }
+    )}
     fetchPokeData();
+    return ()=>{isMounted.current=false;}
     },[])
 
+    const loadMore=()=>{
+    setVisibleResults(prevResults=>prevResults+48);
+    }
     return (
         <div className="cards-container">
         <div className="cards-catalog-left"></div>
         <div className="cards-catalog">
-        {results.map((result,i)=>{
+        {results.slice(0,visibleResults).map((result,i)=>{
         return  result.name.includes(inputFromRedux.payload.searchInput) ?
         <PokemonCard key={i} id={i}
         photo={getImagesByName(result.name)} name={result.name}/> : null})}  
+        {inputFromRedux.payload.searchInput.length>0 || visibleResults>=maxVisibleResults ? null :
+        <Button color="secondary" variant="contained" size="large" onClick={loadMore}>LOAD MORE</Button>}
         </div>
         <div className="cards-catalog-right"></div>
         <div className="footer"><Footer/></div>
